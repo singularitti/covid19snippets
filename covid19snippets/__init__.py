44e01c8c2e2cf38469f7e19bcc6d087162bf0e66
singularitti <singularitti@outlook.com>
2020-04-27 23:22:01 -0400

    Update covid19snippets/__init__.py

--- covid19snippets/__init__.py
+++ covid19snippets/__init__.py
@@ -1,1 +1,27 @@
 __version__ = '0.1.0'
+
+import datetime
+import urllib.request
+from io import BytesIO
+
+import pandas as pd
+
+
+def get_rawdata():
+    url = 'https://covidtracking.com/api/v1/states/daily.csv'
+    response = urllib.request.urlopen(url)
+    return response.read()  # a `bytes` object
+
+
+def get_state_by_abbr():
+    return 1
+
+
+def parsedate(s: str):
+    return datetime.datetime.strptime(s, '%Y%m%d')
+
+
+def getdata():
+    raw = get_rawdata()
+    df = pd.read_csv(BytesIO(raw), encoding='utf8', dtype={'date': str, 'state': str})
+    return df
